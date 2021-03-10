c70344857b12d0e923d7d85d9d1f0ec80c96a0f0
DESKTOP-9PCJ6PM\artem <artik.kostko@gmail.com>
2021-03-10 23:46:23 +0300

    States

--- src/Components/filterPanel.js
+++ src/Components/filterPanel.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const FilterPanelComponent = ({ toDo, done }) => {
-    return (
-        <div className="filterPanel d-flex justify-content-between my-3">
-            <div className="btn-group" role="group" aria-label="Basic radio toggle button group">
-                <input type="radio" className="btn-check" name="btnradio" id="btnradio1" autoComplete="off"/>
-                <label className="btn btn-outline-primary" htmlFor="btnradio1">All</label>
+export default class FilterPanelComponent extends Component {
 
-                <input type="radio" className="btn-check" name="btnradio" id="btnradio2" autoComplete="off"/>
-                <label className="btn btn-outline-primary" htmlFor="btnradio2">Active</label>
+    render () {
+        const { toDo, done } = this.props;
 
-                <input type="radio" className="btn-check" name="btnradio" id="btnradio3" autoComplete="off"/>
-                <label className="btn btn-outline-primary" htmlFor="btnradio3">Done</label>
+        return (
+            <div className="filterPanel d-flex justify-content-between my-3">
+                <div className="btn-group" role="group" aria-label="Basic radio toggle button group">
+                    <input type="radio" className="btn-check" name="btnradio" id="btnradio1" autoComplete="off"/>
+                    <label className="btn btn-outline-primary" htmlFor="btnradio1">All</label>
+    
+                    <input type="radio" className="btn-check" name="btnradio" id="btnradio2" autoComplete="off"/>
+                    <label className="btn btn-outline-primary" htmlFor="btnradio2">Active</label>
+    
+                    <input type="radio" className="btn-check" name="btnradio" id="btnradio3" autoComplete="off"/>
+                    <label className="btn btn-outline-primary" htmlFor="btnradio3">Done</label>
+                </div>
+    
+                <div className="ItemStatusFilter">
+                    <span className="fs-5 text-secondary">{ toDo } more to do, { done } done</span>
+                </div>
             </div>
-
-            <div className="ItemStatusFilter">
-                <span className="fs-5 text-secondary">{ toDo } more to do, { done } done</span>
-            </div>
-        </div>
-    );
-};
-
-export default FilterPanelComponent;
+        );
+    };
+}
--- src/Components/toDo-list-item.js
+++ src/Components/toDo-list-item.js
@@ -1,29 +1,58 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../Styles/CSS/mainStyles.css';
 
-const ToDoListItem = ({ label, important = false }) => {
-    const importantStyle = {
-      color: important ? 'steelBlue' : 'black',
-      fontWeight: important ? '700' : 'normal'
-    };
+export default class ToDoListItem extends Component {
+  state = {
+    done: false,
+    important: false
+  };
+
+  onDoneClick = () => {
+    this.setState({
+      done: true
+    });
+  };
+
+  onImportantClick = () => {
+    this.setState({
+      important: true
+    });
+  };
+
+  render() {
+    const { label } = this.props;
+    const { done, important } = this.state;
+
+    let className = 'toDo-list-item';
+
+    if(done) {
+      className += ' done';
+    }
+    
+    if(important) {
+      className += ' important';
+    }
 
     return (
-      <div className = "toDo-list-item">
-        <span className="d-flex justify-content-between align-items-center" style = { importantStyle }> 
+      <div className = { className }>
+        <span className="toDo-list-item-label d-flex justify-content-between align-items-center"> 
           { label } 
         
           <div className="interaction-buttons">
-            <button className="btn btn-outline-danger btn-sm mx-1" type="button">
-              <i className="fa fa-trash-o"/>
+            <button className="btn btn-outline-success btn-sm mx-1 p-1" type="button" onClick={ this.onDoneClick }>
+              <i className="fa fa-check"/>
             </button>
 
-            <button className="btn btn-outline-success btn-sm mx-1" type="button">
+            <button className="btn btn-outline-warning btn-sm mx-1 p-1" type="button" onClick={ this.onImportantClick }>
               <i className="fa fa-exclamation"/>
+            </button>
+
+            <button className="btn btn-outline-danger btn-sm mx-1 p-1" type="button">
+              <i className="fa fa-trash-o"/>
             </button>
           </div>
         </span>
       </div>
     );
-};
-
-export default ToDoListItem;
+  };
+}
